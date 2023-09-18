0d1991150de9ba7227c0ac0a4bbc0e08d536bcc5
Han-Ya-Jun <1581532052@qq.com>
2023-09-18 22:06:09 +0800

    test gpg

--- src/dashboard/apigateway/manage.py
+++ src/dashboard/apigateway/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apigateway.settings")
+    os.environ.setdefault("2DJANGO_SETTINGS_MODULE", "apigateway.settings")
     import django
 
     django.setup()
